830cd86837ef61dcf49ca619c6eaafa4ba7c8050
justathem <aymen@amplitude.ventures>
2025-02-19 13:29:48 +0300

    Endpoint for retrieving all available phases along with info about each sub-phase

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("start_analysis/", views.start_analysis, name="start_analysis"),
     path("get_analysis_status/", views.get_analysis_status, name="get_analysis_status"),
     path("get_analysis_results/", views.get_analysis_results, name="get_analysis_results"),
+    path('get_phases/', views.get_phases, name="get_phases")
 ]
